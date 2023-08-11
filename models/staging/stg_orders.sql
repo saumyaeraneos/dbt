c6b7f052f4d7d33200c1cbfa249050f8635f5bb6
saumya.kapoor@eraneos.com <saumya.kapoor@eraneos.com>
2023-08-11 15:45:37 +0000

    bug fix in stage_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -7,16 +7,18 @@ o.ordersellingprice - o.ordercostprice as orderprofit,
 o.ordersellingprice,
 o.ordercostprice,
 --from raw customer
+c.customerid,
 c.customername,
 c.segment,
 c.country,
 -- from raw product
+p.productid,
 p.category,
 p.productname,
 p.subcategory
 from 
 {{ ref('raw_orders') }} o
-left join {{ ref('raw_customer') }} r
-on o.customerid = r.customerid
-left join {{ ref('raw_product') }} product
+left join {{ ref('raw_customer') }} c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} p
 on o.productid = p.productid
