99a8a70ee652a7e075f50e02ed24b8977eb5c1e3
saumya.kapoor@eraneos.com <saumya.kapoor@eraneos.com>
2023-08-11 15:54:41 +0000

    commit changes till stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,24 +1,21 @@
-select 
-o.orderid,
-o.orderdate,
-o.shipdate,
-o.shipmode,
-o.ordersellingprice - o.ordercostprice as orderprofit,
-o.ordersellingprice,
-o.ordercostprice,
---from raw customer
-c.customerid,
-c.customername,
-c.segment,
-c.country,
--- from raw product
-p.productid,
-p.category,
-p.productname,
-p.subcategory
-from 
-{{ ref('raw_orders') }} o
-left join {{ ref('raw_customer') }} c
-on o.customerid = c.customerid
-left join {{ ref('raw_product') }} p
-on o.productid = p.productid
+select
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordersellingprice - o.ordercostprice as orderprofit,
+    o.ordersellingprice,
+    o.ordercostprice,
+    -- from raw customer
+    c.customerid,
+    c.customername,
+    c.segment,
+    c.country,
+    -- from raw product
+    p.productid,
+    p.category,
+    p.productname,
+    p.subcategory
+from {{ ref("raw_orders") }} o
+left join {{ ref("raw_customer") }} c on o.customerid = c.customerid
+left join {{ ref("raw_product") }} p on o.productid = p.productid
